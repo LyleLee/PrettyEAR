fcb87b84e27a6de2280f6b16482657cdbe622104
lixianfa <lixianfa.official@gmail.com>
2021-12-12 09:16:21 +0800

    auto format

--- europe.js
+++ europe.js
@@ -3,70 +3,70 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 
 (async () => {
-	try {
-		const response = await got('https://eur-lex.europa.eu/eli/reg/2021/821');
-		//console.log(response.body);
-		//=> '<!doctype html> ...'
+    try {
+        const response = await got('https://eur-lex.europa.eu/eli/reg/2021/821');
+        //console.log(response.body);
+        //=> '<!doctype html> ...'
 
         const $ = cheerio.load(response.body);
-        
+
 
         var deep = 0;
 
 
-function ParaseTable(t, eccn) {
+        function ParaseTable(t, eccn) {
 
-    deep++;
+            deep++;
 
-    $(t).find('tbody').first().find('tr').first().each(function(rindex) {
+            $(t).find('tbody').first().find('tr').first().each(function (rindex) {
 
-        var trString = "";
+                var trString = "";
 
-        $(this).children('td').each(function(tdindex) {
-            //console.log($(this).children(":first").html());
-            if (trString != "") {
-                trString = trString + ' '
-            }
-            trString = trString + $(this).children().first().text().replace(/(\r\n|\n|\r)/gm, "").trim();
-        });
+                $(this).children('td').each(function (tdindex) {
+                    //console.log($(this).children(":first").html());
+                    if (trString != "") {
+                        trString = trString + ' '
+                    }
+                    trString = trString + $(this).children().first().text().replace(/(\r\n|\n|\r)/gm, "").trim();
+                });
 
-        for (var i = 1; i < deep; i++) {
-            trString = '  ' + trString;
-        }
-        
-        
-        fs.writeFile(eccn+'.txt', trString+'\n', {flag:'a'}, (err) => {
-            if (err) {
-                console.log('write error when precessing::', eccn)
-                throw err;
-            }
-        });
+                for (var i = 1; i < deep; i++) {
+                    trString = '  ' + trString;
+                }
 
-        //console.log(trString);
 
-        $(this).children('td').each(function(tdindex) {
+                fs.writeFile(eccn + '.txt', trString + '\n', { flag: 'a' }, (err) => {
+                    if (err) {
+                        console.log('write error when precessing::', eccn)
+                        throw err;
+                    }
+                });
 
-            $(this).children('table').each(function(index) {
-                ParaseTable($(this), eccn);
+                //console.log(trString);
+
+                $(this).children('td').each(function (tdindex) {
+
+                    $(this).children('table').each(function (index) {
+                        ParaseTable($(this), eccn);
+                    });
+                });
             });
-        });
-    });
 
-    deep--;
+            deep--;
 
-}
+        }
 
-        $('#L_2021206EN\\.01002501 > div > table').each(function(index) {
+        $('#L_2021206EN\\.01002501 > div > table').each(function (index) {
 
             var eccn = /^([0-9][A-E]\d{3})|(EAR99)\b/.exec($(this).text().replace(/(\r\n|\n|\r)/gm, "").trim());
             if (eccn != null) {
-                fs.exists(eccn[1]+'.txt', function(exists) {
-                    if(exists){
-                        fs.unlinkSync(eccn[1]+'.txt');
+                fs.exists(eccn[1] + '.txt', function (exists) {
+                    if (exists) {
+                        fs.unlinkSync(eccn[1] + '.txt');
                     }
                 });
                 //fs.unlinkSync(eccn[1]+'.txt');
-                ParaseTable($(this),eccn[1]);
+                ParaseTable($(this), eccn[1]);
             }
 
         });
@@ -74,9 +74,9 @@ function ParaseTable(t, eccn) {
 
 
 
-	} catch (error) {
-		console.log(error.response.body);
-		//=> 'Internal server error ...'
-	}
+    } catch (error) {
+        console.log(error.response.body);
+        //=> 'Internal server error ...'
+    }
 })();
 
