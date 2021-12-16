f05f6bf8b69ea5d7b691b923cb3197ae5ff7fce7
lixianfa <lixianfa.official@gmail.com>
2021-12-16 23:04:26 +0800

    use array to save data

--- test3E002.js
+++ test3E002.js
@@ -1,6 +1,6 @@
 function loadScript(src) {
-    return new Promise(function(resolve, reject) {
-        var s;
+    return new Promise(function (resolve, reject) {
+        let s;
         s = document.createElement('script');
         s.src = src;
         s.onload = resolve;
@@ -21,71 +21,71 @@ load();
 
 console.clear();
 
-var deep = 0;
-var eccnStak = [];
+let deep = 0;
+let eccnStak = [];
+let dataMap = [];
 
 function ParaseTable(t) {
 
     deep++;
 
 
-    $(t).find('tbody').first().find('tr').first().each(function(rindex) {
+    $(t).find('tbody').first().find('tr').first().each(function (rindex) {// every table has only one tr
 
-        var trString = "";
-        var nextTable = [];
+        let trString = "";
+        let nextTable = [];
 
-        $(this).children('td').each(function(tdindex) {
+        $(this).children('td').each(function (tdindex) {// every tr has 2 or 3 td
             if (trString != "") {
                 //add one space before index
                 trString = trString + ' '
             }
-            $(this).children().each(function(pindex) {
-                // td has either p or table
-                if ($(this).is('table') == true) {
+            $(this).children().each(function (pindex) { // td has either lines of p or table
+                if ($(this).is('table') == true) { 
                     nextTable.push($(this));
                 } else {
-                    var thistdText = $(this).text().replace(/(\r\n|\n|\r)/gm, "").trim();
-                    var testResult = /^([0-9][A-E]\d{3})|(EAR99)|^([a-z]\.)|^([1-9]\.)\b/.exec(thistdText);
-                    if (testResult) {
-                       eccnStak.push(testResult[0]);
+                    let thistdText = $(this).text().replace(/(\r\n|\n|\r)/gm, "").trim();// tdtext can be 3E002, a,b,c or a line of conetent text.
+                    let testResult = /^([0-9][A-E]\d{3})|(EAR99)|^([a-z]\.)|^([1-9]\.)\b/.exec(thistdText);//match an index
+                    if (testResult) { // deal with index
+                        eccnStak.push(testResult[0]);
                     }
-                    if (/^Technical Note:/.exec(thistdText)) {
+                    if (/^Technical Note:/.exec(thistdText)) { // deal with line of content text
                         trString = trString + '\n' + thistdText;
                     } else {
                         trString = trString + thistdText;
-
                     }
-
                 }
             });
         });
 
-        for (var i = 0; i < deep; i++) {
+        for (let i = 0; i < deep; i++) {
             trString = '  ' + trString;
         }
-        
-        var thiseccn = "";
-        eccnStak.forEach(element => { 
-            thiseccn+=element;
+
+        let thiseccn = "";
+        eccnStak.forEach(element => {
+            thiseccn = thiseccn + '.' + element;
         });
 
-        eccnStak.pop();
-        
-        console.log(thiseccn, trString);
+        //console.log(thiseccn, trString);
+        dataMap.push([thiseccn,trString]);
 
-        nextTable.forEach((element)=>{
+
+        nextTable.forEach((element) => {
             ParaseTable(element);
         }
         )
     });
+    eccnStak.pop();
+    console.log(dataMap);
 
     deep--;
 
 }
 
-$('body > table').each(function(index) {
+$('body > table').each(function (index) {
 
-    var eccn = /^([0-9][A-E]\d{3})|(EAR99)\b/.exec($(this).text().replace(/(\r\n|\n|\r)/gm, "").trim());
+    let eccn = /^([0-9][A-E]\d{3})|(EAR99)\b/.exec($(this).text().replace(/(\r\n|\n|\r)/gm, "").trim());
 
     if (eccn != null) {
         ParaseTable($(this), '');
