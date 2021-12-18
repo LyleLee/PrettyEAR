14f8910fbe9ebe9e7e41200ec006f3535d5f0a8a
lixianfa <lixianfa.official@gmail.com>
2021-12-18 11:04:02 +0800

    parase table with eccn index

--- test3E002.js
+++ test3E002.js
@@ -17,37 +17,40 @@ async function load() {
 
 }
 
-load();
+await load();
 
 console.clear();
 
+
 let deep = 0;
-let eccnStak = [];
 let dataMap = [];
 
-function ParaseTable(t) {
-
+function ParaseTable(t, tableECCN) {
     deep++;
-
-
     $(t).find('tbody').first().find('tr').first().each(function (rindex) {// every table has only one tr
-
         let trString = "";
         let nextTable = [];
-
         $(this).children('td').each(function (tdindex) {// every tr has 2 or 3 td
             if (trString != "") {
                 //add one space before index
                 trString = trString + ' '
             }
             $(this).children().each(function (pindex) { // td has either lines of p or table
-                if ($(this).is('table') == true) { 
+                if ($(this).is('table') == true) {
                     nextTable.push($(this));
                 } else {
                     let thistdText = $(this).text().replace(/(\r\n|\n|\r)/gm, "").trim();// tdtext can be 3E002, a,b,c or a line of conetent text.
-                    let testResult = /^([0-9][A-E]\d{3})|(EAR99)|^([a-z]\.)|^([1-9]\.)\b/.exec(thistdText);//match an index
-                    if (testResult) { // deal with index
-                        eccnStak.push(testResult[0]);
+                    let testResult = /^(\d[A-E]\d{3})|(EAR99)|^([a-z]\.)|^(\d\.)|(Note\s\d:)/.exec(thistdText);//match an index
+                    if (thistdText.length < 8 && testResult) { // deal with index, some long conetent text could begin with an index, that is why we set length limit 8
+                        if (/\d[A-E]\d{3}/.exec(thistdText)) {
+                            tableECCN.push(thistdText + '.'); //3E002.
+                        }
+                        else if (/Note\s\d:/.exec(thistdText)) {
+                            tableECCN.push(thistdText.split(':')[0] + '.'); //Note 1.
+                        }
+                        else {
+                            tableECCN.push(testResult[0]); // a. b. c.
+                        }
                     }
                     if (/^Technical Note:/.exec(thistdText)) { // deal with line of content text
                         trString = trString + '\n' + thistdText;
@@ -62,33 +65,29 @@ function ParaseTable(t) {
             trString = '  ' + trString;
         }
 
-        let thiseccn = "";
-        eccnStak.forEach(element => {
-            thiseccn = thiseccn + '.' + element;
+        let currentECCN = "";
+        tableECCN.forEach(element => {
+            currentECCN = currentECCN + element;
         });
 
-        //console.log(thiseccn, trString);
-        dataMap.push([thiseccn,trString]);
-
+        console.log(currentECCN, trString);
 
         nextTable.forEach((element) => {
-            ParaseTable(element);
-        }
-        )
+            ParaseTable(element, tableECCN);
+        });
+
+        tableECCN.pop();
     });
-    eccnStak.pop();
-    console.log(dataMap);
-
     deep--;
-
 }
 
 $('body > table').each(function (index) {
 
     let eccn = /^([0-9][A-E]\d{3})|(EAR99)\b/.exec($(this).text().replace(/(\r\n|\n|\r)/gm, "").trim());
+    let tableECCN = [];
 
     if (eccn != null) {
-        ParaseTable($(this), '');
+        ParaseTable($(this), tableECCN);
     }
 
 });
