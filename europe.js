21b7be83cdd48459f8570e70f467cc41667fea79
lixianfa <lixianfa.official@gmail.com>
2022-01-23 10:53:04 +0800

    rewrite test3E002 into europe

--- europe.js
+++ europe.js
@@ -8,7 +8,7 @@ console.log('clean old file');
 
 tools.removeOldFile('.');
 
-(async () => {
+(async() => {
     try {
         console.log('fetch url');
         const response = await got('https://eur-lex.europa.eu/eli/reg/2021/821');
@@ -20,63 +20,142 @@ tools.removeOldFile('.');
 
         console.log('html loaded');
 
+        function TableTool(table) {
 
-        var deep = 0;
+            let deep = 0;
+            let dataMap = [];
+            tableECCN = [];
 
+            ParaseTable($(table), tableECCN);
 
-        function ParaseTable(t, eccn) {
+            function GetAllDeepECCN(tableECCN) {
+                let allECCN = "";
 
-            deep++;
+                if (tableECCN.length == 0)
+                    return allECCN;
 
-            $(t).find('tbody').first().find('tr').first().each(function (rindex) {
+                allECCN = tableECCN[0];
+                for (let i = 1; i < tableECCN.length; ++i) {
+                    allECCN += tableECCN[i];
+                }
+                return allECCN;
+            }
 
-                var trString = "";
+            function ReplaceAndTrim(text) {
+                return text.replace(/(\r\n|\n|\r)/gm, "").trim();
+            }
 
-                $(this).children('td').each(function (tdindex) {
-                    //console.log($(this).children(":first").html());
-                    if (trString != "") {
-                        trString = trString + ' '
+            function HasItalic(node) {
+
+                if ($(node).hasClass('oj-italic')) return true;
+
+                let span = $(node).find('span');
+
+                for (let i = 0; i < span.length; i++) {
+                    if ($(span[i]).hasClass('oj-italic')) {
+                        //console.log("has oj-italic");
+                        return true;
                     }
-                    trString = trString + $(this).children().first().text().replace(/(\r\n|\n|\r)/gm, "").trim();
-                });
+                }
+                return false;
+            }
 
-                for (var i = 1; i < deep; i++) {
-                    trString = '  ' + trString;
+            function ParaseTable(t, tableECCN) {
+                deep++;
+
+                let tableData = [];
+                let longECCN = '';
+                let miniECCN = '';
+                let currentText = "";
+                let tr = $(t).find('tbody').first().find('tr').first(); //every table has only 1 tr
+                let tds = $(tr).children('td'); // every tr has 2 or 3 td
+                let td = null; // table, text, p
+                let tdECCN = null;
+
+                if (tds.length == 3) { // it is a top level table, et: 3E002
+                    tdECCN = tds[1];
+                    //longECCN = longECCN + '.' + ReplaceAndTrim($(tds[1]).text());
+                    td = tds[2];
+                } else { // not top level table.
+                    tdECCN = tds[0];
+                    td = tds[1];
                 }
 
+                miniECCN = ReplaceAndTrim($(tdECCN).text()); // a or b or c ..
 
-                fs.writeFile(eccn + '.txt', trString + '\n', { flag: 'a' }, (err) => {
-                    if (err) {
-                        console.log('write error when precessing::', eccn)
-                        throw err;
+                if (miniECCN[miniECCN.length - 1] != '.') {
+                    miniECCN += '.'
+                }
+                tableECCN.push(miniECCN);
+
+                longECCN = GetAllDeepECCN(tableECCN);
+
+                let tdchilds = $(td).children();
+
+                if (tdchilds.length == 1) // only text
+                {
+                    currentText = ReplaceAndTrim($(td).text());
+                    dataMap.push([longECCN, currentText]);
+                    console.log(longECCN, '--', currentText);
+                } else if (tdchilds.length > 1) { // text,table,p.annotion
+
+                    currentText = ReplaceAndTrim($(tdchilds[0]).text()); // a.A 'vector processor unit' designed to perform more than
+                    console.log(longECCN, '--', currentText);
+                    dataMap.push([longECCN, currentText]);
+
+                    let techNote = '';
+                    let belongAnnotation = false;
+                    for (let i = 1; i < tdchilds.length; ++i) {
+                        if ($(tdchilds[i]).is('p') && $(tdchilds[i]).hasClass('oj-ti-annotation')) { //Technical Note: //Technical Notes:
+                            techNote = ReplaceAndTrim($(tdchilds[i]).text());
+                            currentText = techNote;
+                            tableECCN.push(techNote) // //Technical Note: //Technical Notes:
+                            longECCN = GetAllDeepECCN(tableECCN);
+                            console.log(longECCN, '--', currentText);
+                            dataMap.push([longECCN, currentText]);
+                            belongAnnotation = true;
+                            continue;
+                        }
+                        if ($(tdchilds[i]).is('table') && belongAnnotation == true && HasItalic(tdchilds[i])) { //note table
+                            ParaseTable($(tdchilds[i]), tableECCN);
+                            continue;
+                        }
+                        if ($(tdchilds[i]).is('p') && belongAnnotation == true && HasItalic(tdchilds[i])) { //note paragrah
+                            currentText = $(tdchilds[i]).text();
+                            console.log(longECCN, '--', currentText);
+                            dataMap.push([longECCN, currentText]);
+                            continue;
+                        }
+                        if ($(tdchilds[i]).is('table') && belongAnnotation == false) {
+                            ParaseTable($(tdchilds[i]), tableECCN);
+                        }
                     }
-                });
+                    if (belongAnnotation == true) {
+                        tableECCN.pop(); // pop techNote when loop end.
+                    }
 
-                //console.log(trString);
+                } else {
+                    console.log('error');
+                }
+                tableECCN.pop();
+            }
 
-                $(this).children('td').each(function (tdindex) {
-
-                    $(this).children('table').each(function (index) {
-                        ParaseTable($(this), eccn);
-                    });
-                });
-            });
-
-            deep--;
-
+            return dataMap;
         }
 
-        $('#L_2021206EN\\.01002501 > div > table').each(function (index) {
+
+        $('#L_2021206EN\\.01002501 > div > table').each(function(index) {
 
             var eccn = /^([0-9][A-E]\d{3})|(EAR99)\b/.exec($(this).text().replace(/(\r\n|\n|\r)/gm, "").trim());
             if (eccn != null) {
                 //fs.unlinkSync(eccn[1]+'.txt');
-                ParaseTable($(this), eccn[1]);
+                let datar = TableTool($(this));
+
+                console.log('things in datamap');
+                console.log(datar);
             }
 
         });
-
-
 
 
     } catch (error) {
@@ -84,4 +163,3 @@ tools.removeOldFile('.');
         //=> 'Internal server error ...'
     }
 })();
-
